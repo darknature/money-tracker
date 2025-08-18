50804876e7e7aad4452c5e94467428d0ed5940e7
darknature <ignat.kkovalev@gmail.com>
2025-08-18 22:13:40 +1000

    logger init

--- cmd/money-tracker/main.go
+++ cmd/money-tracker/main.go
@@ -1,16 +1,61 @@
 package main
 
-import "money-tracker/internal/config"
+import (
+	"log/slog"
+	"os"
+
+	"money-tracker/internal/config"
+	"money-tracker/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
 	// init config
 	cfg := config.MustLoad()
 
 	// init log
+	log := setupLogger(cfg.Env)
+	log.Info("starting app", slog.String("env", cfg.Env))
+	log.Debug("debug message are enabled")
 
 	// init storage
 
 	// init router
 
 	// run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
