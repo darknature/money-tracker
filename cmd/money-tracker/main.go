c9641beec1e9e85b166b78240dc9ce7fd73368f4
darknature <ignat.kkovalev@gmail.com>
2025-08-18 22:01:35 +1000

    init cfg

--- cmd/money-tracker/main.go
+++ cmd/money-tracker/main.go
@@ -1,12 +1,15 @@
 package main
 
+import "money-tracker/internal/config"
+
 func main() {
 	// init config
+	cfg := config.MustLoad()
 
 	// init log
- 
+
 	// init storage
-	
+
 	// init router
 
 	// run server
